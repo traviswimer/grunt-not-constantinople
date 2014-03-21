2e2a21ff97701e7e27d31cc1bea2648720cf24c4
Travis Wimer <accounts@traviswimer.com>
2014-03-21 14:15:33 -0400

    Remove coverage files from GIT

--- test/coverage/instrument/app/test.js
+++
@@ -1,9 +0,0 @@
-
-var __cov_8a$24DnqGhoaurny5dr5$w = (Function('return this'))();
-if (!__cov_8a$24DnqGhoaurny5dr5$w.__coverage__) { __cov_8a$24DnqGhoaurny5dr5$w.__coverage__ = {}; }
-__cov_8a$24DnqGhoaurny5dr5$w = __cov_8a$24DnqGhoaurny5dr5$w.__coverage__;
-if (!(__cov_8a$24DnqGhoaurny5dr5$w['app/test.js'])) {
-   __cov_8a$24DnqGhoaurny5dr5$w['app/test.js'] = {"path":"app/test.js","s":{"1":0,"2":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"(anonymous_1)","line":2,"loc":{"start":{"line":2,"column":17},"end":{"line":2,"column":27}}}},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":5,"column":2}},"2":{"start":{"line":3,"column":2},"end":{"line":3,"column":14}}},"branchMap":{}};
-}
-__cov_8a$24DnqGhoaurny5dr5$w = __cov_8a$24DnqGhoaurny5dr5$w['app/test.js'];
-__cov_8a$24DnqGhoaurny5dr5$w.s['1']++;module.exports={simpleFunction:function(){__cov_8a$24DnqGhoaurny5dr5$w.f['1']++;__cov_8a$24DnqGhoaurny5dr5$w.s['2']++;return true;}};
